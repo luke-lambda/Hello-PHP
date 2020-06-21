a0683a2711d8bebd5eaca733ca19a703dfd4791a
lucas lambda <lucas@pop-os.localdomain>
2020-06-21 18:16:52 -0300

    Adiciona algoritmo para dar hello de acordo com localizacao do ip

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 
-$curl = curl_init();
-$ipUrl = "http://ip-api.com/json/?fields=countryCode";
+$cc = getURL("http://ip-api.com/json/?fields=countryCode", 'countryCode');
+$x = getURL("https://fourtonfish.com/hellosalut/?cc={$cc}", 'hello');
+print PHP_EOL . html_entity_decode($x) . PHP_EOL;
 
-curl_setopt($curl, CURLOPT_URL, $ipUrl);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+function getURL($url, $strField)
+{
+    $curl = curl_init();
+    
+    // cURL GET
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-$resp = curl_exec($curl);
-curl_close($curl);
+    $resp = curl_exec($curl);
+    curl_close($curl);
 
-$countryCode = json_decode($resp)->{'countryCode'};
+    return json_decode($resp)->{$strField};
+};
